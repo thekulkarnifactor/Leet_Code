a00f84e5646b2a69da3fb44822a551e042aad77a
Harish Rajendra Kulkarni <76848576+thekulkarnifactor@users.noreply.github.com>
2024-04-17 15:56:37 +0530

    UPDATE: 70_Climbing_Stairs

---
+++ Python/70_Climbing_Stairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: return n
+
+        prev1 = 3
+        prev2 = 2
+        cur = 0
+
+        for _ in range(3, n):
+            cur = prev1 + prev2
+            prev2 = prev1
+            prev1 = cur
+        
+        return cur
+
+def main():
+    solution = Solution()
+    ans = solution.climbStairs(5)
+    print(ans)
+if __name__ == "__main__":
+    main() 
